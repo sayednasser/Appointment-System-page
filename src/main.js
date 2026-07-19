fcf7111802b53fb2d234a77bc950cb2b6199bd11
sayedmahdy716-glitch <sayedmahdy716@gmail.com>
2026-07-19 18:27:35 +0300

    Update: Configure app for Vercel serverless environment2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
- 
+// import { bootstrap } from "./app.bootstrap.js";
+
+                 
+//  bootstrap();
+
+import { bootstrap } from "./app.bootstrap.js";
+
+// Vercel يحتاج أن يجد export default ليتمكن من تشغيل الكود
+export default async (req, res) => {
+    const app = await bootstrap();
+    app(req, res);
+};
